3fd8ff5b0495469861c8925703d8a0efb9c21b17
Brendan <bmurray@niche.com>
2018-09-28 15:50:38 -0400

    Add iOS tests

---
+++ test/ios/test.js
@@ -0,0 +1,10 @@
+suite('iOS tests - ', () => {
+
+    test('Should pass', async () => {
+      browser.waitForExist("~Hi it's the Niche Mobile App", 5000)
+    })
+
+    test('Should fail', async () => {
+      browser.waitForExist("~Hi it's not the Niche Mobile App", 5000)
+    })
+})
